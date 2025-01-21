d7822b33f3f5be07bd51390bc909bc7aa79059f1
Kiran <88050617+Kiran9223@users.noreply.github.com>
2025-01-20 17:13:46 -0800

    ReAct steps 1,2,3

--- agent.py
+++ agent.py
@@ -2,6 +2,7 @@ import google.generativeai as genai
 from tools import getCurrentWeather, getLocation
 from prompts import prompt
 from secret_key import GEMINI_API_KEY
+import re
 
 genai.configure(api_key=GEMINI_API_KEY)
 
@@ -29,7 +30,26 @@ def agent(query):
         ]
     )
     response = chat.send_message(query)
-    print(response.text)
+    #  /**
+    #  * PLAN:
+    #  * 1. Split the string on the newline character \n
+    #  * 2. Search through the array of strings for one that has "Action:"
+    #  * 3. Parse the action (function and parameter) from the string
+    #  * 4. Calling the function
+    #  * 5. Add an "Obversation" message with the results of the function call
+    #  */
+    split_response = response.text.split("\n")
+    actionStr=""
+    match=""
+    for s in split_response:
+        match = re.search(r"Action:\s*(.*?):", s)
+        if match:
+            actionStr = s
+            break
+    print(actionStr)
+    print(match.group(1))
+    
+    
 
 agent("Please give me some ideas for activities to do this afternoon.")
 
